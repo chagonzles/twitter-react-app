dfb31e6e172f15c55f9f7b2262fc08e87fe48aaf
Charlene Gonzales <charlene.gonzales@ivantageapps.com>
2017-08-18 16:58:19 +0800

    Update header, footer and layout and remove unnecessary files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,41 +1,37 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
-import Link from '../Link';
-import Navigation from '../Navigation';
-import logoUrl from './logo-small.png';
-import logoUrl2x from './logo-small@2x.png';
+
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem, Button} from 'react-bootstrap';
 
 class Header extends React.Component {
   render() {
     return (
       <div className={s.root}>
-        <div className={s.container}>
-          <Navigation />
-          <Link className={s.brand} to="/">
-            <img
-              src={logoUrl}
-              srcSet={`${logoUrl2x} 2x`}
-              width="38"
-              height="38"
-              alt="React"
-            />
-            <span className={s.brandTxt}>Your Company</span>
-          </Link>
-          <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
-          </div>
-        </div>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Twitter-App</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              {/**<NavItem eventKey={1} href="#">Link</NavItem>
+                            <NavItem eventKey={2} href="#">Link</NavItem>
+                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                              <MenuItem eventKey={3.1}>Action</MenuItem>
+                              <MenuItem eventKey={3.2}>Another action</MenuItem>
+                              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                              <MenuItem divider />
+                              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                            </NavDropdown>**/}
+            </Nav>
+            <Nav pullRight>
+              <Button bsStyle="success" className={s.signInButton}>Sign in With Twitter</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }

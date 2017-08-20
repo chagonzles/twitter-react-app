dcb9e02586b612687203c98bdf88a7b1887d02e8
Charlene Gonzales <charlene.gonzales@ivantageapps.com>
2017-08-20 09:16:34 +0800

    Update user info component

--- src/components/Profile/UserInfo.js
+++ src/components/Profile/UserInfo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import request from 'superagent';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Profile.css';
-import {Image} from 'react-bootstrap';
+import {Row,Col,Image} from 'react-bootstrap';
 import AppConstants from '../../constants/AppConstants';
 import Session from '../../core/Session';
 
@@ -31,9 +31,9 @@ class UserInfo extends React.Component {
       var name = this.state.user.name ? this.state.user.name : '';
       var profileImage = this.state.user.profile_image_url ? this.state.user.profile_image_url : null;
       var screenName = this.state.user.screen_name ? this.state.user.screen_name : '';
-      var tweetsCount = this.state.user.statuses_count ? this.state.user_statuses_count : '';
+      var tweetsCount = this.state.user.statuses_count ? this.state.user.statuses_count : 0;
       var followingCount = this.state.user.friends_count ? this.state.user.friends_count : '';
-      var followersCount = this.state.user.followersCount ? this.state.user.followersCount : '';
+      var followersCount = this.state.user.followers_count ? this.state.user.followers_count : 0;
     }
     return (
       <div className={s.userInfoContainer}>
@@ -42,7 +42,21 @@ class UserInfo extends React.Component {
         </div>
         
         <div className={s.fullName}>{name}</div>
-        <div className={s.userName}>{screenName}</div>
+        <div className={s.userName}>{screenName ? '@' +screenName : ''}</div>
+        <Row className={s.userStats}> 
+          <Col xs={4}>
+            <div className={s.statName}>Tweets</div>
+            <div className={s.statValue}>{tweetsCount}</div>
+          </Col>
+          <Col xs={4}>
+            <div className={s.statName}>Following</div>
+            <div className={s.statValue}>{followingCount}</div>
+          </Col>
+          <Col xs={4}>
+            <div className={s.statName}>Followers</div>
+            <div className={s.statValue}>{followersCount}</div>
+          </Col>
+        </Row>
       </div>
     );
   }

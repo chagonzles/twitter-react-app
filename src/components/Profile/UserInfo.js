d07d73ac77dae0cb33659f9c35da42ec9e9b4c8d
Charlene Gonzales <charlene.gonzales@ivantageapps.com>
2017-08-20 09:59:06 +0800

    Fix loading of profile image

--- src/components/Profile/UserInfo.js
+++ src/components/Profile/UserInfo.js
@@ -38,7 +38,11 @@ class UserInfo extends React.Component {
     return (
       <div className={s.userInfoContainer}>
         <div className={s.thumbnailContainer}>
-          <Image className={s.thumbnail} src={profileImage} circle responsive/>
+          { profileImage ?
+            <Image className={s.thumbnail} src={profileImage} circle responsive/>
+            :
+            <Image className={s.noThumbnail}/>
+          }
         </div>
         
         <div className={s.fullName}>{name}</div>

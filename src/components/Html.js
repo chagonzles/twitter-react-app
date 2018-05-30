3cc85b5178bfb8bcc6cf71c0f1a073cfc0f4447d
chagonzles <charlene.gonzles@gmail.com>
2018-05-30 12:32:29 +0800

    Fix bootstrap theme css link

--- src/components/Html.js
+++ src/components/Html.js
@@ -46,7 +46,7 @@ class Html extends React.Component {
           </title>
           <meta name="description" content={description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="stylesheet" href="https://bootswatch.com/paper/bootstrap.min.css" />
+          <link rel="stylesheet" href="https://bootswatch.com/3/paper/bootstrap.min.css" />
           {scripts.map(script =>
             <link key={script} rel="preload" href={script} as="script" />,
           )}
